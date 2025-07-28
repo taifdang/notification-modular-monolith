ef1cc4e36be723dab73109739ef8b21063686722
LAPTOP-J20BGGNG\taida <taidangduc1503@gmail.com>
2025-07-28 22:26:10 +0700

    chore: delete trash file

--- src/Modules/Notifications/Hookpay.Modules.Notifications.Core/Messages/Features/SendMessage/SignalrChannelQueue.cs
+++ src/Modules/Notifications/Hookpay.Modules.Notifications.Core/Messages/Features/SendMessage/SignalrChannelQueue.cs
@@ -10,11 +10,13 @@ public record SignalrChannelQueue (int target, Alert alert);
 
 public class SignalrChannelQueueHandler : IConsumer<SignalrChannelQueue>
 {
-    private readonly INotificationHubService _hub;
+    private readonly INotificationHubService _hub;   
     private readonly ILogger<SignalrChannelQueueHandler> _logger;
+
     public SignalrChannelQueueHandler(
         INotificationHubService notificationHubService,
-        ILogger<SignalrChannelQueueHandler> logger)
+        ILogger<SignalrChannelQueueHandler> logger
+        )
     {
         _hub = notificationHubService;
         _logger = logger;
@@ -25,8 +27,6 @@ public class SignalrChannelQueueHandler : IConsumer<SignalrChannelQueue>
         try
         {
             var data = context.Message.alert;
-            //await Task.Delay(100);
-            //await _hub.SendAllAsync(context.Message.body);
             await _hub.SendPersonalAsync(context.Message.target.ToString(), data.Body);
             _logger.LogInformation($"Message with {data.Title} = {data.Body} is processed");
 
--- src/Shared/Hookpay.Shared/Jwt/JwtExtensions.cs
+++ src/Shared/Hookpay.Shared/Jwt/JwtExtensions.cs
@@ -41,6 +41,8 @@ public static class JwtExtensions
                     ValidateLifetime = true,
                     ValidateIssuerSigningKey = true,
                 };
+
+                //ref: https://learn.microsoft.com/en-us/aspnet/core/signalr/authn-and-authz?view=aspnetcore-5.0#use-authorization-handlers-to-customize-hub-method-authorization
                 options.Events = new JwtBearerEvents
                 {
                     OnMessageReceived = (context) =>
